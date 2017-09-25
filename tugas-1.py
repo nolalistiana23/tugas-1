caa25a8f7ae3a75573a591432caf4f3662767c73
nolalistiana23 <32118479+nolalistiana23@users.noreply.github.com>
2017-09-26 06:22:21 +0700

    tugas
    tugas python

---
+++ tugas-1.py
@@ -0,0 +1,11 @@
+Nama = "Nola Listiana Devi"
+NIM  = "04317012"
+Umur = 18
+print ("Nama =", (Nama))
+print ("NIM  =", (NIM))
+print ("Umur =")
+print (Umur)
+print ("5 tahun lagi =")
+print (Umur+5)
+
+
